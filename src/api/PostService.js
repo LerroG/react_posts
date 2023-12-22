0bf2de8cce960225e2169dabe39a01492e03b161
unknown <ssakoks@mail.ru>
2023-12-22 23:13:07 +0500

    page by id

--- src/api/PostService.js
+++ src/api/PostService.js
@@ -2,14 +2,22 @@ import axios from 'axios';
 
 export class PostService {
 	static async getAll(limit = 10, page = 1) {
-			const response = await axios.get(
-				'https://jsonplaceholder.typicode.com/posts', {
-					params: {
-						_limit: limit,
-						_page: page
-					}
-				}
-			);
-			return response;
+		const response = await axios.get(
+			'https://jsonplaceholder.typicode.com/posts',
+			{
+				params: {
+					_limit: limit,
+					_page: page,
+				},
+			}
+		);
+		return response;
+	}
+
+	static async getById(id) {
+		const response = await axios.get(
+			'https://jsonplaceholder.typicode.com/posts/' + id
+		);
+		return response;
 	}
 }
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useFetching } from '../hooks/useFetching';
+import { PostService } from '../api/PostService';
+import MyLoader from '../components/UI/loader/MyLoader';
 
 const PostIdPage = () => {
 	const params = useParams();
-	return <div>Страница поста</div>;
+	const [post, setPost] = useState({});
+	const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+		const response = await PostService.getById(id);
+		setPost(response.data);
+	});
+
+	useEffect(() => {
+		fetchPostById(params.id);
+	}, []);
+
+	return (
+		<div>
+			<h1>Страница поста {params.id}</h1>
+			{isLoading ? (
+				<MyLoader />
+			) : (
+				<div>
+					{post.id}. {post.title}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default PostIdPage;
